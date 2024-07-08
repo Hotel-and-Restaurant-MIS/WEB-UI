a7bb9af9b821dd9a43e67904adfab674456a3ccd
Your Name <you@example.com>
2024-07-08 20:09:09 +0530

    header

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard.jsx';
+
+function App() {
+    return (
+        <div>Hello World!</div>
+    );
+}
+
+export default App;
