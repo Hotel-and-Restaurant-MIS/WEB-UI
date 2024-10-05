d305dfed0c4fc1952f90524083fa4f4fabb44766
Sunera Sandaruwan <sandaruwankds.21@uom.lk>
2024-10-05 22:12:00 +0530

    api reconfig

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 export const urlInstance = axios.create({
-  baseURL: "/api",
-  headers: {
-    'Content-Type': 'application/json',  // Ensure correct content type for POST
-    'Accept': 'application/json',
-  }
+
+  baseURL: "http://34.136.246.135"
+  
+  // baseURL: "/api",
+  // headers: {
+  //   'Content-Type': 'application/json',  // Ensure correct content type for POST
+  //   'Accept': 'application/json',
+  // }
 });
 
