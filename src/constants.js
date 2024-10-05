7fed617fb1928ae9ba212d0515f692174f207458
Sunera Sandaruwan <sandaruwankds.21@uom.lk>
2024-10-05 22:13:58 +0530

    branch created

--- src/constants.js
+++ src/constants.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export const urlInstance = axios.create({
 
-  baseURL: "http://34.136.246.135"
-  
-  // baseURL: "/api",
-  // headers: {
-  //   'Content-Type': 'application/json',  // Ensure correct content type for POST
-  //   'Accept': 'application/json',
-  // }
+  // baseURL: "http://34.136.246.135"
+
+  baseURL: "/api",
+  headers: {
+    'Content-Type': 'application/json',  // Ensure correct content type for POST
+    'Accept': 'application/json',
+  }
 });
 
