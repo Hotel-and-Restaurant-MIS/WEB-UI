ac339401c51c45799a5dcdacc7088bee6a9d0e31
Sunera Sandaruwan <sandaruwankds.21@uom.lk>
2024-10-05 20:04:57 +0530

    reconfig netlify

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,9 @@ import axios from 'axios';
 
 export const urlInstance = axios.create({
   baseURL: "/api",
+  headers: {
+    'Content-Type': 'application/json',  // Ensure correct content type for POST
+    'Accept': 'application/json',
+  }
 });
 
